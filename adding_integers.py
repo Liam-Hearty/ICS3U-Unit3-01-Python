33c8e634058a65472ccde1143029feb9f881d4e3
Ubuntu <ubuntu@ip-172-31-17-81.ec2.internal>
2019-09-26 17:27:21 +0000

    added program

---
+++ adding_integers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Hearty
+# Created on: September 2019
+# This program adds two integers together.
+
+
+def main():
+    # This program adds two integers together.
+
+    # input
+    first_integer = int(input("Enter the first integer of the adding " +
+                              "equation: "))
+    second_integer = int(input("Enter the second integer of the adding " +
+                               "equation: "))
+
+    # process
+    total = first_integer + second_integer
+
+    # output
+    print("")
+    print("Your sum is {}".format(total))
+
+
+if __name__ == "__main__":
+    main()
